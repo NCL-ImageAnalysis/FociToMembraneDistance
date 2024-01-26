0e87f877a8cb5e73a829037f768beb6a209b16b5
James Grimshaw <jameswgrimshaw@gmail.com>
2024-01-26 10:16:56 +0000

    comments

--- Foci_to_Membrane_Distance.py
+++ Foci_to_Membrane_Distance.py
@@ -77,6 +77,7 @@ WatershedBool = False
 # Analyze particles settings-v
 Cell_Sizes = "200-inf"
 Cell_Circularity = "0.00-1.00"
+# This is to catch any foci outside of the cell
 Roi_Enlargement = 3
 #----------------------------^
 
